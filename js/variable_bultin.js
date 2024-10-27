5806024300e24f6f074001348ce4471c7e2c61ee
quangminh-coder <tthu53397@gmail.com>
2024-10-27 21:21:38 +0700

    Buoi2

---
+++ js/variable_bultin.js
@@ -0,0 +1,18 @@
+// input , output ---------------
+//alert("this is a notification")
+//const isStudent = confirm("Are you a student?");
+//console.log(isStudent)
+//const age = prompt("How old are you");
+//console.log(typeof age);
+//varible (scope)
+//for (var index = 0; index < 10; index+++) {
+ //console.log(index)}
+//console.log(index);
+//const a = 10;
+const object = { name: "abc" , age:10 };
+object.name = "xyz"
+console.table(object)
+console.log(typeof firstname);
+//template string
+console.log("my full name is ${firstname}")
+const fullname = firstname
